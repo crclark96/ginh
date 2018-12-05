54e2f32fa4afef950236a94940e07b58e4c302c9
Collin Clark <crclark96@gmail.com>
2018-12-05 11:35:19 -0500

    commitment issues

---
+++ cmd_historgram.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+declare -a COUNTS FREQ CMDS
+LINE_LEN=80
+NUM_ENTRIES=15
+CHART_CHAR='='
+HISTFILE="$HOME/.bash_history"
+OPTIND=1 # reset getopts
+
+function show_help {
+  echo "usage: $0 [-h] [-n entries] [-f hist_file] [-c chart_char] [-l line_len]"
+}
+
+function separator {
+  for (( N=0; N<=$LINE_LEN; N++ ))
+  do
+    printf "-"
+  done
+  printf "\n"
+}
+
+while getopts "h?n:f:c:l:" opt; do
+  case "$opt" in
+  h|\?)
+    show_help
+    exit 0
+    ;;
+  n)
+    NUM_ENTRIES=$OPTARG
+    ;;
+  f)
+    HISTFILE=$OPTARG
+    ;;
+  c)
+    CHART_CHAR="$OPTARG"
+    ;;
+  l)
+    LINE_LEN=$OPTARG
+    ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+[ "${1:-}" = "--" ] && shift
+
+echo "entries=$NUM_ENTRIES, file=$HISTFILE, char=$CHART_CHAR, len=$LINE_LEN"
+
+
+for (( N=0; N<=$NUM_ENTRIES; N++ ))
+# gather counts and cmds
+do
+  CMDS[$N]=$(cat $HISTFILE | sort | uniq -c | sort -n | tail -n `expr $N + 1` | head -n 1)
+  COUNTS[$N]=$(echo ${CMDS[$N]} | awk '{print $1}')
+done
+
+for (( N=0; N<=`expr $NUM_ENTRIES - 1`; N++ ))
+# calculate frequencies
+do 
+  (( FREQ[N]=COUNTS[N] * LINE_LEN / COUNTS[0] ))
+done
+
+separator
+
+for (( N=0; N<=`expr $NUM_ENTRIES - 1`; N++ ))
+do
+  for (( M=0; M<=FREQ[$N]; M++ ))
+  do
+    printf "$CHART_CHAR"
+  done
+  printf "  "
+  for M in ${CMDS[$N]}
+  do
+    printf "%s " $M
+  done 
+  printf "\n"
+done
+
+separator
+
