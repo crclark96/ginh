2192c818c4bbf272fb3ab2a3338118bb6100c90d
Ryan Geary <rtgnj42@gmail.com>
2018-12-05 22:03:12 -0500

    Add -p option for left-aligned labels

--- cmd_historgram.sh
+++ cmd_historgram.sh
@@ -9,7 +9,7 @@ OPTIND=1 # reset getopts
 MAX_LEN=0
 
 function show_help {
-  echo "usage: $0 [-h] [-n entries] [-f hist_file] [-c chart_char] [-l line_len]"
+  echo "usage: $0 [-h] [-n entries] [-f hist_file] [-c chart_char] [-p] [-l line_len]"
 }
 
 function separator {
@@ -20,7 +20,7 @@ function separator {
   printf "\n"
 }
 
-while getopts "h?n:f:c:l:" opt
+while getopts "h?n:f:c:l:p" opt
 do
   case "$opt" in
   h|\?)
@@ -39,6 +39,9 @@ do
   l)
     LINE_LEN=$OPTARG
     ;;
+  p)
+    MAX_LEN=1
+    ;;
   esac
 done
 
@@ -54,29 +57,42 @@ for (( N=0; N<=$NUM_ENTRIES; N++ ))
 do
   CMDS[$N]=$(cat $HISTFILE | sort | uniq -c | sort -n | tail -n `expr $N + 1` | head -n 1)
   COUNTS[$N]=$(echo ${CMDS[$N]} | awk '{print $1}')
-  MAX_LEN=$((
-  ${#CMDS[$N]} > MAX_LEN ?
-    ${#CMDS[$N]}:
-    MAX_LEN
-  ))
+  if [ $MAX_LEN -gt 0 ]; then
+    MAX_LEN=$((
+    ${#CMDS[$N]} > $MAX_LEN ?
+      ${#CMDS[$N]}:
+      $MAX_LEN
+    ))
+  fi
 done
 
 for (( N=0; N<=`expr $NUM_ENTRIES - 1`; N++ ))
 # calculate frequencies
 do
-  (( FREQ[N]=COUNTS[N] * LINE_LEN / COUNTS[0] ))
+  (( FREQ[N]=COUNTS[N] * `expr $LINE_LEN - $MAX_LEN` / COUNTS[0] ))
 done
 
 separator
 
 for (( N=0; N<=`expr $NUM_ENTRIES - 1`; N++ ))
 do
+  if [ $MAX_LEN -gt 0 ]; then
+    for (( M=0; M<=MAX_LEN-${#CMDS[$N]}; M++ ))
+    do
+      printf " "
+    done
+    printf "%s " $(echo ${CMDS[$N]} | cut -d' ' -f2-)
+  fi
   for (( M=0; M<=FREQ[$N]; M++ ))
   do
     printf "$CHART_CHAR"
   done
   printf "  "
-  printf "%s " ${CMDS[$N]}
+  if [ $MAX_LEN -gt 0 ]; then
+    printf "%s" $(echo ${CMDS[$N]} | awk '{print $1}')
+  else
+    printf "%s " ${CMDS[$N]}
+  fi
   printf "\n"
 done
 
