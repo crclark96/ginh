f73b76583df265b1eca05d62bed5a4378af5d852
Collin Clark <crclark96@gmail.com>
2018-12-16 22:45:03 -0500

    make @theryangeary 's print format the default fit all lines within a hard max

--- cmd_historgram.sh
+++ cmd_historgram.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 declare -a COUNTS FREQ CMDS
-LINE_LEN=`expr $(/usr/bin/tput cols) - 10`
+LINE_LEN=`expr $(/usr/bin/tput cols) - 2` # get terminal width
 NUM_ENTRIES=15
 CHART_CHAR='='
 HISTFILE="$HOME/.bash_history"
@@ -9,7 +9,7 @@ OPTIND=1 # reset getopts
 MAX_LEN=0
 
 function show_help {
-  echo "usage: $0 [-h] [-n entries] [-f hist_file] [-c chart_char] [-p] [-l line_len]"
+  echo "usage: $0 [-h] [-n entries] [-f hist_file] [-c chart_char] [-l line_len]"
 }
 
 function separator {
@@ -20,7 +20,7 @@ function separator {
   printf "\n"
 }
 
-while getopts "h?n:f:c:l:p" opt
+while getopts "h?n:f:c:l" opt
 do
   case "$opt" in
   h|\?)
@@ -39,9 +39,6 @@ do
   l)
     LINE_LEN=$OPTARG
     ;;
-  p)
-    MAX_LEN=1
-    ;;
   esac
 done
 
@@ -51,52 +48,45 @@ shift $((OPTIND-1))
 
 echo "entries=$NUM_ENTRIES, file=$HISTFILE, char=$CHART_CHAR, len=$LINE_LEN"
 
-
 for (( N=0; N<=$NUM_ENTRIES; N++ ))
 # gather counts and cmds
 do
   CMDS[$N]=$(cat $HISTFILE | awk '{print $1}' | sort | uniq -c | sort -n | tail -n `expr $N + 1` | head -n 1)
   COUNTS[$N]=$(echo ${CMDS[$N]} | awk '{print $1}')
-  if [ $MAX_LEN -gt 0 ]; then
-    S=$(echo ${CMDS[$N]} | cut -d' ' -f2-)
-    MAX_LEN=$((
-    ${#S} > $MAX_LEN ?
-      ${#S}:
-      $MAX_LEN
-    ))
-  fi
+  S=$(echo ${CMDS[$N]} | cut -d' ' -f2-)
+  MAX_LEN=$((
+  ${#S} > $MAX_LEN ?
+    ${#S}:
+    $MAX_LEN
+  ))
 done
 
-MAX_LEN=$(($MAX_LEN > 0 ? $MAX_LEN+1: 0))
+MAX_LEN=$(($MAX_LEN + 1))
 
 for (( N=0; N<=`expr $NUM_ENTRIES - 1`; N++ ))
 # calculate frequencies
 do
-  (( FREQ[N]=COUNTS[N] * `expr $LINE_LEN - $MAX_LEN` / COUNTS[0] ))
+  (( FREQ[N]=COUNTS[N] * `expr $LINE_LEN - $MAX_LEN - ${#COUNTS[0]} - 2` / COUNTS[0] ))
 done
 
 separator
 
 for (( N=0; N<=`expr $NUM_ENTRIES - 1`; N++ ))
 do
-  if [ $MAX_LEN -gt 0 ]; then
-    S=$(echo ${CMDS[$N]} | cut -d' ' -f2-)
-    for (( M=0; M<=MAX_LEN-${#S} - 2; M++ ))
-    do
-      printf " "
-    done
-    printf "%s " $(echo ${CMDS[$N]} | cut -d' ' -f2-)
-  fi
+  S=$(echo ${CMDS[$N]} | cut -d' ' -f2-)
+  for (( M=0; M<=MAX_LEN-${#S} - 2; M++ ))
+  do
+    printf " "
+  done
+  printf "%s " $(echo ${CMDS[$N]} | cut -d' ' -f2-)
+  
   for (( M=0; M<=FREQ[$N]; M++ ))
   do
     printf "$CHART_CHAR"
   done
   printf "  "
-  if [ $MAX_LEN -gt 0 ]; then
-    printf "%s" $(echo ${CMDS[$N]} | awk '{print $1}')
-  else
-    printf "%s " ${CMDS[$N]}
-  fi
+  printf "%s" $(echo ${CMDS[$N]} | awk '{print $1}')
+  
   printf "\n"
 done
 
